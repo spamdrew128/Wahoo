c4a54482ee63db19407547336787589c1a466db2
spamdrew128 <spamdrew128@gmail.com>
2023-05-27 12:18:06 -0400

    things compile

--- build.rs
+++ build.rs
@@ -1,7 +1,16 @@
 mod build_script_dependencies;
 
+use std::path::PathBuf;
+use std::fs::File;
+use std::io::Write;
+use std::io::BufWriter;
+
 use crate::build_script_dependencies::magic_table_builder::generate_magic_table;
+
 fn main() {
-    let contents = generate_magic_table();
-    println!("Hello, world!");
+    let mut out_dir: PathBuf = std::env::var("OUT_DIR").unwrap().into();
+    out_dir.push("magic_table.rs");
+    
+    let mut out_file = BufWriter::new(File::create(out_dir).unwrap());
+    write!(&mut out_file, "{}", generate_magic_table()).unwrap();
 }
