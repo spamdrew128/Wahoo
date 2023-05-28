afb1a1e404e8d08ecf16d946663da2b86bbfefc8
spamdrew128 <spamdrew128@gmail.com>
2023-05-28 13:50:08 -0400

    added stages

--- src/movegen.rs
+++ src/movegen.rs
@@ -1,16 +1,34 @@
 use crate::chess_move::Move;
+use crate::tuple_constants_enum;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct MoveStage(u8);
+
+impl MoveStage {
+    tuple_constants_enum!(Self,
+        CAPTURES,
+        QUIET
+    );
+
+    const fn new(data: u8) -> Self {
+        Self(data)
+    }
+}
 
 const MOVE_LIST_SIZE: usize = u8::MAX as usize;
 struct MoveGenerator {
     movelist: [Move; MOVE_LIST_SIZE],
     len: usize,
+
+    pub stage: MoveStage,
 }
 
 impl MoveGenerator {
     const fn new() -> Self {
         Self {
             movelist: [Move::nullmove(); MOVE_LIST_SIZE],
-            len: 0
+            len: 0,
+            stage: MoveStage::new(0)
         }
     }
 }
