a1ab0365c61384c7d84ca8181106d01da9c4d048
spamdrew128 <spamdrew128@gmail.com>
2023-06-01 21:12:47 -0400

    created file

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod chess_move;
 mod magic;
 mod movegen;
 mod perft;
+mod uci;
 mod util_macros;
 
 fn main() {
---
+++ src/uci.rs
@@ -0,0 +1,11 @@
+use crate::board_representation::Board;
+
+struct Uci {
+    board: Board
+}
+
+impl Uci {
+    fn await_instructions(&self) {
+        
+    }
+}
