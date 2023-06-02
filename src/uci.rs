27376f2a640f84680fb550292ea7541b0a98aee9
spamdrew128 <spamdrew128@gmail.com>
2023-06-01 21:27:55 -0400

    omw

--- src/uci.rs
+++ src/uci.rs
@@ -1,11 +1,18 @@
-use crate::board_representation::Board;
+use crate::board_representation::{Board, START_FEN};
 
 struct Uci {
-    board: Board
+    board: Board,
 }
 
 impl Uci {
+    fn new() -> Self {
+        Self { board: Board::from_fen(START_FEN) }
+    }
+
     fn await_instructions(&self) {
-        
+        let mut input = String::new();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("UCI Input Failure");
     }
 }
