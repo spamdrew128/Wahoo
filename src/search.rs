a0f753879f591be1242bc67864ff72760fdea87e
spamdrew128 <spamdrew128@gmail.com>
2023-06-07 12:51:15 -0400

    fixed div by 0

--- src/search.rs
+++ src/search.rs
@@ -22,7 +22,7 @@ pub struct Searcher {
 
 fn report_search_info(score: EvalScore, nodes: Nodes, depth: Depth, stopwatch: Instant) {
     let elapsed = stopwatch.elapsed().as_millis();
-    let nps = (u128::from(nodes) * 1_000_000) / stopwatch.elapsed().as_micros();
+    let nps = (u128::from(nodes) * 1_000_000) / stopwatch.elapsed().as_micros().max(1);
     print!("info ");
     println!("score cp {score} nodes {nodes} time {elapsed} nps {nps} depth {depth}");
 }
