0a85bd0b797d446fe9c0ebca77c6e6de7e605d56
spamdrew128 <spamdrew128@gmail.com>
2023-06-12 09:42:42 -0400

    made engine library

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -6,7 +6,7 @@
 #![allow(dead_code)] // remove later
 
 mod attacks;
-mod bench;
+pub mod bench;
 mod board_representation;
 mod chess_move;
 mod evaluation;
@@ -16,22 +16,7 @@ mod perft;
 mod pv_table;
 mod search;
 mod time_management;
-mod uci;
+pub mod uci;
 mod util_macros;
 mod zobrist;
 mod zobrist_stack;
-
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    for arg in args {
-        if arg == "bench" {
-            bench::bench();
-            return;
-        }
-    }
-
-    std::env::set_var("RUST_BACKTRACE", "1");
-
-    let mut uci_handler = uci::UciHandler::new();
-    while matches!(uci_handler.execute_instructions(), uci::ProgramStatus::Run) {}
-}
---
+++ uci_loop/src/main.rs
@@ -0,0 +1,16 @@
+use engine::{uci, bench};
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    for arg in args {
+        if arg == "bench" {
+            bench::bench();
+            return;
+        }
+    }
+
+    std::env::set_var("RUST_BACKTRACE", "1");
+
+    let mut uci_handler = uci::UciHandler::new();
+    while matches!(uci_handler.execute_instructions(), uci::ProgramStatus::Run) {}
+}
