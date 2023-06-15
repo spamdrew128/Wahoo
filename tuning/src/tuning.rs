a45445799f9c752c23be35b1118d2f6090db0074
spamdrew128 <spamdrew128@gmail.com>
2023-06-15 17:27:32 -0400

    increased check freq

--- tuning/src/tuning.rs
+++ tuning/src/tuning.rs
@@ -101,7 +101,7 @@ pub struct Tuner {
 impl Tuner {
     const K: f64 = 0.006634;
     const CONVERGENCE_DELTA: f64 = 1e-9;
-    const CONVERGENCE_CHECK_FREQ: u32 = 25;
+    const CONVERGENCE_CHECK_FREQ: u32 = 50;
     const MAX_EPOCHS: u32 = 20000;
 
     fn new_weights() -> TunerVec {
@@ -136,6 +136,7 @@ impl Tuner {
             let board = Board::from_fen(fen);
             self.entries.push(Entry::new(&board, game_result));
         }
+        println!("Loaded file: begin tuning...\n");
     }
 
     pub fn reset_gradient(&mut self) {
