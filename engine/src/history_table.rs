7e6294ba33fc6e727d2dd03570a66045314af482
spamdrew128 <spamdrew128@gmail.com>
2023-06-17 18:20:46 -0400

    added ager

--- engine/src/history_table.rs
+++ engine/src/history_table.rs
@@ -8,11 +8,23 @@ struct History {
 }
 
 impl History {
-    fn history_score(&self, board: &Board, mv: Move) -> i16 {
+    const fn new() -> Self {
+        Self {
+            scores: [[[0; NUM_COLORS as usize]; NUM_PIECES as usize]; NUM_SQUARES as usize],
+        }
+    }
+
+    fn score(&self, board: &Board, mv: Move) -> i16 {
         let piece = board.piece_on_sq(mv.from()).as_index();
         let to = mv.to().as_index();
         let color = board.color_to_move.as_index();
 
         self.scores[color][piece][to]
+    }
+
+    fn age_scores(&mut self) {
+        for score in self.scores.iter_mut().flatten().flatten() {
+            *score /= 2;
+        }
     }
 }
