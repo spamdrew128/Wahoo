7042416594cce9643325295052c951d1b6106da0
spamdrew128 <spamdrew128@gmail.com>
2023-06-19 15:00:16 -0400

    removed unneeded test

--- engine/src/chess_move.rs
+++ engine/src/chess_move.rs
@@ -287,12 +287,4 @@ mod tests {
             }
         }
     }
-
-    #[test]
-    fn is_pseudo_1() {
-        let board =
-            Board::from_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
-        let mv = Move::new(Square::D4, Square::D2, Flag::DOUBLE_PUSH);
-        assert!(!mv.is_pseudolegal(&board));
-    }
 }
