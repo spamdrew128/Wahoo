8f6a2dbaa48e9fe3b9645e4a9ecd6abf630a2974
spamdrew128 <spamdrew128@gmail.com>
2023-06-19 22:55:33 -0400

    pain

--- engine/src/transposition_table.rs
+++ engine/src/transposition_table.rs
@@ -1,40 +1,50 @@
-// use std::sync::atomic::{AtomicU64, Ordering};
+use std::sync::atomic::{AtomicU64, Ordering};
 
-use crate::{tuple_constants_enum, chess_move::Move, search::Depth, evaluation::EvalScore};
+use crate::{chess_move::Move, evaluation::EvalScore, search::Depth, tuple_constants_enum};
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, Default, Copy, Clone, PartialEq, Eq)]
 struct TTFlag(u8);
 
 impl TTFlag {
-    tuple_constants_enum!(Self,
-        UNINITIALIZED,
-        LOWER_BOUND,
-        EXACT,
-        UPPER_BOUND
-    );
+    tuple_constants_enum!(Self, UNINITIALIZED, LOWER_BOUND, EXACT, UPPER_BOUND);
 
     const fn new(data: u8) -> Self {
         Self(data)
     }
 }
 
+#[derive(Debug, Copy, Clone)]
 #[repr(C)]
 struct TTEntry {
-    flag: TTFlag, // 1 byte
-    depth: Depth, // 1 byte
-    best_move: Move, // 2 byte
+    flag: TTFlag,     // 1 byte
+    depth: Depth,     // 1 byte
+    best_move: Move,  // 2 byte
     score: EvalScore, // 2 byte
-    key: u16, // 2 byte
+    key: u16,         // 2 byte
 }
 
-impl From<u64> for TTEntry {
-    fn from(data: u64) -> Self {
+impl TTEntry {
+    const BYTES: usize = 8;
+
+    const fn new(flag: TTFlag, depth: Depth, best_move: Move, score: EvalScore, key: u16) -> Self {
+        Self {
+            flag,
+            depth,
+            best_move,
+            score,
+            key,
+        }
+    }
+}
+
+impl From<AtomicU64> for TTEntry {
+    fn from(data: AtomicU64) -> Self {
         // SAFETY: This is safe because all fields of TTEntry are (at base) integral types,
         unsafe { std::mem::transmute(data) }
     }
 }
 
-impl From<TTEntry> for u64 {
+impl From<TTEntry> for AtomicU64 {
     fn from(entry: TTEntry) -> Self {
         // SAFETY: This is safe because all bitpatterns of `u64` are valid.
         unsafe { std::mem::transmute(entry) }
@@ -42,5 +52,18 @@ impl From<TTEntry> for u64 {
 }
 
 struct TranspositionTable {
+    table: Vec<AtomicU64>,
+}
 
+impl TranspositionTable {
+    fn new(megabytes: usize) -> Self {
+        const BYTES_PER_MB: usize = 1024 * 1024;
+
+        let bytes = megabytes * BYTES_PER_MB;
+        let entries = bytes / TTEntry::BYTES;
+        let mut table = vec![];
+        table.resize_with(entries, AtomicU64::default);
+
+        Self { table }
+    }
 }
