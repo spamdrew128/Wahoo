e4fa3dc587e0368cfe96aeb30b6f62186d51e9d2
spamdrew128 <spamdrew128@gmail.com>
2023-06-19 22:11:02 -0400

    crabgrab

--- engine/src/transposition_table.rs
+++ engine/src/transposition_table.rs
@@ -1,6 +1,6 @@
 // use std::sync::atomic::{AtomicU64, Ordering};
 
-use crate::tuple_constants_enum;
+use crate::{tuple_constants_enum, chess_move::Move, search::Depth, evaluation::EvalScore};
 
 #[derive(Debug, PartialEq, Eq)]
 struct TTFlag(u8);
@@ -18,8 +18,27 @@ impl TTFlag {
     }
 }
 
+#[repr(C)]
 struct TTEntry {
+    flag: TTFlag, // 1 byte
+    depth: Depth, // 1 byte
+    best_move: Move, // 2 byte
+    score: EvalScore, // 2 byte
+    key: u16, // 2 byte
+}
 
+impl From<u64> for TTEntry {
+    fn from(data: u64) -> Self {
+        // SAFETY: This is safe because all fields of TTEntry are (at base) integral types,
+        unsafe { std::mem::transmute(data) }
+    }
+}
+
+impl From<TTEntry> for u64 {
+    fn from(entry: TTEntry) -> Self {
+        // SAFETY: This is safe because all bitpatterns of `u64` are valid.
+        unsafe { std::mem::transmute(entry) }
+    }
 }
 
 struct TranspositionTable {
