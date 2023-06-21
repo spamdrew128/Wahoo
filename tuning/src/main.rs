c47229b0cccb23d3206b43b5a2d0f5716f94ed29
spamdrew128 <spamdrew128@gmail.com>
2023-06-21 19:15:28 -0400

    printing

--- tuning/src/main.rs
+++ tuning/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let file_name: &str = args.get(1).expect("expected data file to read!");
 
-    let mut tuner = tuning::Tuner::new(true);
+    let mut tuner = tuning::Tuner::new(false);
     tuner.load_from_file(file_name);
     tuner.train();
 }
