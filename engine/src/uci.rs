6c4db0ae3018b4b98176494b655455021227c1a8
spamdrew128 <spamdrew128@gmail.com>
2023-07-01 18:51:12 -0400

    use one stop flag

--- engine/src/uci.rs
+++ engine/src/uci.rs
@@ -9,10 +9,7 @@ use crate::{
     zobrist_stack::ZobristStack,
 };
 
-use std::{
-    sync::atomic::{AtomicBool, Ordering},
-    thread,
-};
+use std::thread;
 
 enum UciCommand {
     Uci,
@@ -278,15 +275,13 @@ impl UciHandler {
                 let mut searcher =
                     Searcher::new(search_limits, &self.zobrist_stack, &self.history, &self.tt);
 
-                let done_searching: AtomicBool = false.into();
                 thread::scope(|s| {
                     s.spawn(|| {
                         searcher.go(&self.board, true);
-                        done_searching.store(true, Ordering::SeqCst);
                         searcher.search_complete_actions(&mut self.history);
                     });
 
-                    Self::handle_stop_and_quit(&mut self.stored_message, &done_searching);
+                    Self::handle_stop_and_quit(&mut self.stored_message);
                 });
             }
             UciCommand::SetOptionOverhead(overhead) => {
@@ -299,7 +294,7 @@ impl UciHandler {
         }
     }
 
-    fn handle_stop_and_quit(stored_message: &mut Option<String>, done_searching: &AtomicBool) {
+    fn handle_stop_and_quit(stored_message: &mut Option<String>) {
         loop {
             let buffer = Self::read_uci_input();
 
@@ -311,11 +306,11 @@ impl UciHandler {
                     return;
                 }
                 _ => {
-                    if done_searching.load(Ordering::SeqCst) {
+                    if search::stop_flag_is_set() {
                         *stored_message = Some(buffer);
                         return;
                     }
-                    eprintln!("Cannot handle command \"{buffer}\" while searching");
+                    eprintln!("Cannot handle command \"{}\" while searching", buffer.trim());
                 }
             };
         }
