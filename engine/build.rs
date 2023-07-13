67a2dfeb19a6096228e895b238fba99b04d0ada2
spamdrew128 <spamdrew128@gmail.com>
2023-07-13 10:37:33 -0400

    fixed build

--- engine/build.rs
+++ engine/build.rs
@@ -23,6 +23,7 @@ fn build_fathom() {
     cc.file("./fathom/src/tbprobe.c");
     cc.include("./fathom/src/");
     cc.define("_CRT_SECURE_NO_WARNINGS", None);
+    cc.flag("-march=native");
 
     // MSVC doesn't support stdatomic.h, so use clang on Windows
     if env::consts::OS == "windows" {
