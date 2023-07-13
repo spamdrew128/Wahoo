58d7416fdedc7a6d941811389ea49b32bda6a854
spamdrew128 <spamdrew128@gmail.com>
2023-07-13 12:44:44 -0400

    added prober

--- engine/src/evaluation.rs
+++ engine/src/evaluation.rs
@@ -27,6 +27,8 @@ pub type EvalScore = i32;
 pub const INF: EvalScore = (i16::MAX - 10) as i32;
 pub const EVAL_MAX: EvalScore = INF - 1;
 pub const MATE_THRESHOLD: EvalScore = EVAL_MAX - (MAX_PLY as i32);
+pub const TB_WIN: EvalScore = MATE_THRESHOLD - 1000;
+pub const TB_LOSS: EvalScore = -TB_WIN;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub struct ScoreTuple(EvalScore, EvalScore);
--- engine/src/tablebase/probe.rs
+++ engine/src/tablebase/probe.rs
@@ -1,6 +1,11 @@
 use std::ffi::CString;
 
-use super::bindings::{tb_free, tb_init};
+use crate::{
+    board_representation::{Board, Color, Piece},
+    evaluation::EvalScore,
+};
+
+use super::bindings::{tb_free, tb_init, tb_probe_wdl_impl};
 
 pub fn init_tablebase(path: &str) {
     unsafe {
@@ -13,4 +18,27 @@ pub fn free_tablebase() {
     unsafe {
         tb_free();
     }
+}
+
+pub fn probe_wdl(board: &Board) -> Option<EvalScore> {
+    if (board.halfmoves != 0) || board.castle_rights.not_empty() {
+        return None;
+    }
+
+    unsafe {
+        let wdl = tb_probe_wdl_impl(
+            board.all[Color::White.as_index()].as_u64(),
+            board.all[Color::Black.as_index()].as_u64(),
+            board.pieces[Piece::KING.as_index()].as_u64(),
+            board.pieces[Piece::QUEEN.as_index()].as_u64(),
+            board.pieces[Piece::ROOK.as_index()].as_u64(),
+            board.pieces[Piece::BISHOP.as_index()].as_u64(),
+            board.pieces[Piece::KNIGHT.as_index()].as_u64(),
+            board.pieces[Piece::PAWN.as_index()].as_u64(),
+            0,
+            board.color_to_move == Color::White,
+        );
+    }
+
+    None
 }
