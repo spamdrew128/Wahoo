d35a3e8ba7bf1d7c93e61feee4b5dc4980afee6d
spamdrew128 <spamdrew128@gmail.com>
2023-07-13 11:50:54 -0400

    added free

--- engine/src/tablebase/probe.rs
+++ engine/src/tablebase/probe.rs
@@ -1,13 +1,16 @@
 use std::ffi::CString;
 
-use super::bindings::tb_init;
+use super::bindings::{tb_free, tb_init};
 
 pub fn init_tablebase(path: &str) {
     unsafe {
         let syzygy_path = CString::new(path).unwrap();
-        assert!(
-            tb_init(syzygy_path.as_ptr()),
-            "TB failed to initalize"
-        );
+        assert!(tb_init(syzygy_path.as_ptr()), "TB failed to initalize");
+    }
+}
+
+pub fn free_tablebase() {
+    unsafe {
+        tb_free();
     }
 }
