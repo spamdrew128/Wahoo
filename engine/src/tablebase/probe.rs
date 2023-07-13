a7d8393df4e3b88b631806b398796f3e556b573e
spamdrew128 <spamdrew128@gmail.com>
2023-07-13 14:07:09 -0400

    added move matcher

--- engine/src/tablebase/probe.rs
+++ engine/src/tablebase/probe.rs
@@ -1,14 +1,53 @@
 use std::{ffi::CString, ptr};
 
 use crate::{
-    board_representation::{Board, Color, Piece},
+    board_representation::{Board, Color, Piece, Square},
+    chess_move::{Flag, Move},
     evaluation::{EvalScore, TB_LOSS_SCORE, TB_WIN_SCORE},
 };
 
 use super::bindings::{
     tb_free, tb_init, tb_probe_root_impl, tb_probe_wdl_impl, TB_BLESSED_LOSS, TB_CURSED_WIN,
-    TB_DRAW, TB_LARGEST, TB_LOSS, TB_WIN,
+    TB_DRAW, TB_LARGEST, TB_LOSS, TB_PROMOTES_BISHOP, TB_PROMOTES_KNIGHT, TB_PROMOTES_QUEEN,
+    TB_PROMOTES_ROOK, TB_RESULT_FROM_MASK, TB_RESULT_FROM_SHIFT, TB_RESULT_PROMOTES_MASK,
+    TB_RESULT_PROMOTES_SHIFT, TB_RESULT_TO_MASK, TB_RESULT_TO_SHIFT, TB_RESULT_WDL_MASK,
+    TB_RESULT_WDL_SHIFT, TB_WIN,
 };
+
+struct SyzygyResult(u32);
+
+impl SyzygyResult {
+    fn score(self) -> EvalScore {
+        let wdl = (self.0 & TB_RESULT_WDL_MASK) >> TB_RESULT_WDL_SHIFT;
+        match wdl {
+            TB_WIN => TB_WIN_SCORE,
+            TB_LOSS => TB_LOSS_SCORE,
+            _ => 0,
+        }
+    }
+
+    fn matches_move(self, mv: Move) -> bool {
+        let from = Square::new(((self.0 & TB_RESULT_FROM_MASK) >> TB_RESULT_FROM_SHIFT) as u8);
+        let to = Square::new(((self.0 & TB_RESULT_TO_MASK) >> TB_RESULT_TO_SHIFT) as u8);
+
+        if mv.to() != to || mv.from() != from {
+            return false;
+        }
+
+        if mv.is_promo() {
+            let promo = (self.0 & TB_RESULT_PROMOTES_MASK) >> TB_RESULT_PROMOTES_SHIFT;
+            match mv.flag() {
+                Flag::KNIGHT_PROMO | Flag::KNIGHT_CAPTURE_PROMO => promo == TB_PROMOTES_KNIGHT,
+                Flag::BISHOP_PROMO | Flag::BISHOP_CAPTURE_PROMO => promo == TB_PROMOTES_BISHOP,
+                Flag::ROOK_PROMO | Flag::ROOK_CAPTURE_PROMO => promo == TB_PROMOTES_ROOK,
+                Flag::QUEEN_PROMO | Flag::QUEEN_CAPTURE_PROMO => promo == TB_PROMOTES_QUEEN,
+                _ => false,
+            }
+        } else {
+            true
+        }
+    }
+}
 
 pub fn init_tablebase(path: &str) {
     unsafe {
@@ -73,7 +112,7 @@ fn probe_root(board: &Board) {
     };
 
     unsafe {
-        let result = tb_probe_root_impl(
+        let result = SyzygyResult(tb_probe_root_impl(
             board.all[Color::White.as_index()].as_u64(),
             board.all[Color::Black.as_index()].as_u64(),
             board.pieces[Piece::KING.as_index()].as_u64(),
@@ -86,6 +125,6 @@ fn probe_root(board: &Board) {
             ep_sq,
             board.color_to_move == Color::White,
             ptr::null_mut(),
-        );
+        ));
     }
 }
