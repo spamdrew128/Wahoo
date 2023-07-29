b7d73069f95311cc43162193278dd3cbdb9f509c
spamdrew128 <spamdrew128@gmail.com>
2023-07-29 02:57:19 -0400

    fmt

--- engine/src/search/see.rs
+++ engine/src/search/see.rs
@@ -5,19 +5,22 @@ use crate::board::{
 
 pub const SEE_VALS: [i32; NUM_PIECES as usize] = [450, 450, 650, 1250, 100, 0];
 
-impl Board {
-    fn gain(&self, mv: Move, victim: Piece) -> i32 {
-        if mv.flag() == Flag::EP {
+impl Move {
+    fn gain(self, victim: Piece) -> i32 {
+        if self.flag() == Flag::EP {
             return SEE_VALS[Piece::PAWN.as_index()];
         }
 
         let base = SEE_VALS[victim.as_index()];
-        if mv.is_promo() {
-            base + SEE_VALS[mv.promo_piece().as_index()] - SEE_VALS[Piece::PAWN.as_index()]
+        if self.is_promo() {
+            base + SEE_VALS[self.promo_piece().as_index()] - SEE_VALS[Piece::PAWN.as_index()]
         } else {
             base
         }
     }
 
-    fn see(&self, mv: Move, attacker: Piece, victim: Piece) {}
+    fn see(self, board: &Board, attacker: Piece, victim: Piece, threshold: i32) {
+        let sq = self.to();
+        let val = self.gain(victim) - threshold;
+    }
 }
