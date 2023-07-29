137a2962349d0e6b91b605002bf61b5077990312
spamdrew128 <spamdrew128@gmail.com>
2023-07-29 19:35:05 -0400

    removed gain

--- engine/src/search/see.rs
+++ engine/src/search/see.rs
@@ -3,24 +3,21 @@ use crate::board::{
     chess_move::{Flag, Move},
 };
 
-pub const SEE_VALS: [i32; NUM_PIECES as usize] = [450, 450, 650, 1250, 100, 0];
+pub const SEE_VALS: [i32; (NUM_PIECES + 1) as usize] = [450, 450, 650, 1250, 100, 0, 0];
 
 impl Move {
-    fn gain(self, victim: Piece) -> i32 {
-        if self.flag() == Flag::EP {
-            return SEE_VALS[Piece::PAWN.as_index()];
-        }
-
-        let base = SEE_VALS[victim.as_index()];
-        if self.is_promo() {
-            base + SEE_VALS[self.promo_piece().as_index()] - SEE_VALS[Piece::PAWN.as_index()]
-        } else {
-            base
-        }
-    }
-
     fn see(self, board: &Board, attacker: Piece, victim: Piece, threshold: i32) {
-        let sq = self.to();
-        let val = self.gain(victim) - threshold;
+        let mut piece = attacker;
+        let mut val = SEE_VALS[victim.as_index()] - threshold;
+
+        val += if self.flag() == Flag::EP {
+            SEE_VALS[Piece::PAWN.as_index()]
+        } else if self.is_promo() {
+            piece = self.promo_piece();
+            SEE_VALS[victim.as_index()] + SEE_VALS[self.promo_piece().as_index()]
+                - SEE_VALS[Piece::PAWN.as_index()]
+        } else {
+            SEE_VALS[victim.as_index()]
+        };
     }
 }
