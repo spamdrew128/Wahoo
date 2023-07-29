a3346fe2b887ca2258cd26ba81e4a76c31f6a94e
spamdrew128 <spamdrew128@gmail.com>
2023-07-29 19:42:03 -0400

    reformat

--- engine/src/search/see.rs
+++ engine/src/search/see.rs
@@ -6,18 +6,27 @@ use crate::board::{
 pub const SEE_VALS: [i32; (NUM_PIECES + 1) as usize] = [450, 450, 650, 1250, 100, 0, 0];
 
 impl Move {
-    fn see(self, board: &Board, attacker: Piece, victim: Piece, threshold: i32) {
-        let mut piece = attacker;
-        let mut val = SEE_VALS[victim.as_index()] - threshold;
+    fn see(self, board: &Board, attacker: Piece, victim: Piece, threshold: i32) -> bool {
+        let sq = self.to();
+        let mut val = -threshold;
+        let mut next = attacker;
 
         val += if self.flag() == Flag::EP {
             SEE_VALS[Piece::PAWN.as_index()]
         } else if self.is_promo() {
-            piece = self.promo_piece();
+            next = self.promo_piece();
             SEE_VALS[victim.as_index()] + SEE_VALS[self.promo_piece().as_index()]
                 - SEE_VALS[Piece::PAWN.as_index()]
         } else {
             SEE_VALS[victim.as_index()]
         };
+
+        // if we captured a higher value piece than we attacked with,
+        // we have positive SEE no matter what
+        if val >= 0 {
+            return true;
+        }
+
+        false
     }
 }
