8e9145edf51cde0356ed3131c3671d2224d92029
Andrew Hockman <spamdrew128@gmail.com>
2023-08-09 11:07:40 -0400

    created net

--- tuning/src/safety_tuning.rs
+++ tuning/src/safety_tuning.rs
@@ -1,1 +1,25 @@
+use engine::eval::{king_safety_net::HIDDEN_LAYER_SIZE, trace::SAFETY_TRACE_LEN};
+
 use crate::tuner_val::S;
+
+pub struct Net {
+    hidden_weights: [[S; HIDDEN_LAYER_SIZE]; SAFETY_TRACE_LEN],
+    hidden_biases: [S; HIDDEN_LAYER_SIZE],
+    hidden_activations: [S; HIDDEN_LAYER_SIZE],
+    output_weights: [S; HIDDEN_LAYER_SIZE],
+    output_bias: S,
+    output_activation: S,
+}
+
+impl Net {
+    pub fn new() -> Self {
+        Self {
+            hidden_weights: [[S::new(0.0 ,0.0); HIDDEN_LAYER_SIZE]; SAFETY_TRACE_LEN],
+            hidden_biases: [S::new(0.0 ,0.0); HIDDEN_LAYER_SIZE],
+            hidden_activations: [S::new(0.0 ,0.0); HIDDEN_LAYER_SIZE],
+            output_weights: [S::new(0.0 ,0.0); HIDDEN_LAYER_SIZE],
+            output_bias: S::new(0.0 ,0.0),
+            output_activation: S::new(0.0 ,0.0),
+        }
+    }
+}
