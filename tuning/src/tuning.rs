fa6a54a39b6744686c0fb2e7c4ed5b5a7689e502
spamdrew128 <spamdrew128@gmail.com>
2023-08-15 10:00:03 -0400

    edit convergence and learn rate

--- tuning/src/tuning.rs
+++ tuning/src/tuning.rs
@@ -183,10 +183,10 @@ pub struct Tuner {
 
 impl Tuner {
     const K: f64 = 0.006634;
-    const CONVERGENCE_DELTA: f64 = 1e-7;
+    const CONVERGENCE_DELTA: f64 = 7e-7;
     const CONVERGENCE_CHECK_FREQ: u32 = 50;
     const MAX_EPOCHS: u32 = 20000;
-    const LEARN_RATE: f64 = 0.01;
+    const LEARN_RATE: f64 = 0.015;
 
     fn new_weights() -> TunerStruct {
         let mut result = TunerStruct::new();
