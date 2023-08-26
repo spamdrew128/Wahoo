433add1bdb434d308f0595aac96443b091a6e5d2
spamdrew128 <spamdrew128@gmail.com>
2023-08-25 23:33:15 -0400

    seperate fn

--- engine/src/eval/drawishness.rs
+++ engine/src/eval/drawishness.rs
@@ -10,25 +10,30 @@ use super::{
     trace::Trace,
 };
 
+fn opp_bishops<const TRACE: bool>(board: &Board, t: &mut Trace) -> ScoreTuple {
+    let w_bishops = board.piece_bb(Piece::BISHOP, Color::White);
+    let b_bishops = board.piece_bb(Piece::BISHOP, Color::Black);
+
+    let light_sq_bishops = w_bishops.union(b_bishops).intersection(Bitboard::LIGHT_SQ);
+
+    if w_bishops.popcount() == 1 && b_bishops.popcount() == 1 && light_sq_bishops.popcount() == 1 {
+        if TRACE {
+            trace_drawishness_update!(t, OppBishop, (), 1);
+        }
+
+        return OPPOSITE_BISHOPS;
+    }
+
+    ScoreTuple::new(0, 0)
+}
+
+fn material_imbalance<const TRACE: bool>(board: &Board, t: &mut Trace) -> ScoreTuple {}
+
 impl ScoreTuple {
     pub fn drawishness_adjustment<const TRACE: bool>(self, board: &Board, t: &mut Trace) -> Self {
         let mut drawishness = Self::new(DRAWISHNESS_SCALE, DRAWISHNESS_SCALE);
 
-        let w_bishops = board.piece_bb(Piece::BISHOP, Color::White);
-        let b_bishops = board.piece_bb(Piece::BISHOP, Color::Black);
-
-        let light_sq_bishops = w_bishops.union(b_bishops).intersection(Bitboard::LIGHT_SQ);
-
-        if w_bishops.popcount() == 1
-            && b_bishops.popcount() == 1
-            && light_sq_bishops.popcount() == 1
-        {
-            drawishness += OPPOSITE_BISHOPS;
-
-            if TRACE {
-                trace_drawishness_update!(t, OppBishop, (), 1);
-            }
-        }
+        drawishness += opp_bishops::<TRACE>(board, t);
 
         drawishness = drawishness.clamp(DRAWISHNESS_SCALE / 4, DRAWISHNESS_SCALE);
         self * drawishness / DRAWISHNESS_SCALE
