8968a3f72f3d781ac76b5762cd34a63cffbd7597
spamdrew128 <spamdrew128@gmail.com>
2023-08-25 22:17:41 -0400

    fmt

--- engine/src/eval/drawishness.rs
+++ engine/src/eval/drawishness.rs
@@ -1,16 +1,24 @@
-use crate::board::board_representation::{Board, Color, Piece};
+use crate::board::board_representation::{Bitboard, Board, Color, Piece};
 
-use super::evaluation::{ScoreTuple, DRAWISHNESS_SCALE};
+use super::{
+    eval_constants::OPPOSITE_BISHOPS,
+    evaluation::{ScoreTuple, DRAWISHNESS_SCALE},
+};
 
 impl ScoreTuple {
     fn drawishness_adjustment(&mut self, board: &Board) {
-        let drawishness = ScoreTuple::new(DRAWISHNESS_SCALE, DRAWISHNESS_SCALE);
+        let mut drawishness = Self::new(DRAWISHNESS_SCALE, DRAWISHNESS_SCALE);
 
         let w_bishops = board.piece_bb(Piece::BISHOP, Color::White);
         let b_bishops = board.piece_bb(Piece::BISHOP, Color::Black);
 
-        if w_bishops.popcount() == 1 && b_bishops.popcount() == 1 && {
+        let light_sq_bishops = w_bishops.union(b_bishops).intersection(Bitboard::LIGHT_SQ);
 
+        if w_bishops.popcount() == 1
+            && b_bishops.popcount() == 1
+            && light_sq_bishops.popcount() == 1
+        {
+            drawishness += OPPOSITE_BISHOPS;
         }
     }
 }
